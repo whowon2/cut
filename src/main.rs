4e67ef5f9fa799db82ea63f9da43565e99726a5a
whowon2 <86119976+whowon2@users.noreply.github.com>
2026-03-26 21:42:50 -0300

    feat: add ratatui and rodio and print input file path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_file = args.get(1).expect("Usage: cargo run <path_to_mp3>");
+
+    println!("{}", input_file)
 }
