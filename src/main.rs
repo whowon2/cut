395f24a99f6d57500d3436761b332fb37eb481e4
whowon2 <86119976+whowon2@users.noreply.github.com>
2026-03-26 21:49:03 -0300

    feat: get duration in seconds

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use std::env;
+use std::{env, error::Error, fs::File, io::BufReader, time::Duration};
 
-fn main() {
+use rodio::{Decoder, Source};
+
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let input_file = args.get(1).expect("Usage: cargo run <path_to_mp3>");
+    let file = File::open(input_file)?;
+    let source = Decoder::new(BufReader::new(file))?;
+    let duration = source.total_duration().unwrap_or(Duration::from_secs(1));
+    let total_secs = duration.as_secs_f64();
 
-    println!("{}", input_file)
+    println!("Path: {}", input_file);
+    println!("Duration: {:?}s", total_secs);
+
+    Ok(())
 }
